ce04d7ba5d50db179259c338a120540fa15d58df
antonioconac <conacheantonio13@gmail.com>
2026-04-27 14:45:06 +0300

    Improvements for CI pipeline

--- conftest.py
+++ conftest.py
@@ -16,6 +16,9 @@ def driver():
     })
     options.add_argument("--disable-save-password-bubble")
     options.add_argument("--disable-features=PasswordLeakDetection")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.saucedemo.com")
     yield driver
